6a44733f195e7255d4f30baa792a1692b8427017
Alyson Takai <shtakai@gmail.com>
2016-07-15 14:53:45 -0700

    $location

--- client/customize.users.controller.js
+++ client/customize.users.controller.js
@@ -1,34 +1,41 @@
 
 angular.module('myApp').controller('customizeUsersController',[
-  'userFactory', function(userFactory){
-  console.log('customizeUsersController start');
+  'userFactory',
+  '$location',
+  function(userFactory, $location){
+    console.log('customizeUsersController start');
+    console.log('location', $location.url());
 
-  this.index = () => {
-    console.log('index start');
-    userFactory.index((data)=>{
-      this.users = data;
-    })
-  }
-  this.create = () => {
-    console.log('C create');
-    userFactory.create(
-      this.firstName,
-      this.lastName,
-      this.language,
-      (data) => {
+    this.index = () => {
+      console.log('index start');
+      console.log('location', $location.url());
+      userFactory.index((data)=>{
         this.users = data;
-      }
-    );
-  }
+      })
+    }
+    this.create = () => {
+      console.log('C create');
+      console.log('location', $location.url());
+      userFactory.create(
+        this.firstName,
+        this.lastName,
+        this.language,
+        (data) => {
+          this.users = data;
+        }
+      );
+    }
 
-  this.delete = (_user) => {
-    console.log('C delete');
-    userFactory.delete(
-      _user,
-      (data) => {
-        this.users = data;
-      }
-    )
-  }
+    this.delete = (_user) => {
+      console.log('C delete');
+      console.log('location', $location.url());
+      userFactory.delete(
+        _user,
+        (data) => {
+          this.users = data;
+          $location.url('/list')
+        }
+      )
+    }
 
-}])
+  }])
